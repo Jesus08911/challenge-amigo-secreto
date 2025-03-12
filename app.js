cea1e16d62c260ff64e73c99bf2d7b2b4f64d2de
Jesus08911 <jesus1.moralesm89@gmail.com>
2025-03-12 05:10:47 -0600

    Se añade funcion actualizarLista()

--- app.js
+++ app.js
@@ -20,3 +20,13 @@ function agregarAmigo() {
     actualizarLista();
     input.value = "";
 }
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
