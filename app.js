0aa1c6b6b87a1a6d5a5b2d062ba3d69cc72e7c0d
Jesus08911 <jesus1.moralesm89@gmail.com>
2025-03-12 05:09:43 -0600

    Se añade funcion agregarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor inserta un nombre");
+        return;
+    }
+
+    if (amigos.includes(nombre)) {
+        alert("Este nombre ya ha sido ingresado");
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
